e74270dd5a300dcd21ca191c07dbba087a1e2274
Rudra Gaur <irudragaur@gmail.com>
2024-03-08 15:47:48 +0530

    Update dbConnect.js

--- backend/src/db/dbConnect.js
+++ backend/src/db/dbConnect.js
@@ -1,5 +1,10 @@
 import { Sequelize } from 'sequelize';
 
+{/* I HAVEN'T CONVERTED THESE CREDS TO ENV PATHS SINCE THESE WERE LOCAL TO MY MACHINE */}
+{/* You need the following: */}
+{/* 1) DB NAME */}
+{/* 2) DB USER */}
+{/* 3) DB PASSWORD */}
 const sequelizeConfig = new Sequelize('urlshortener', 'codewithmero', 'Rudra@123qwe', {
   host: 'localhost',
   dialect: 'mysql'
